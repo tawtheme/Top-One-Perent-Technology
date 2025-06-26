e7717b3313d009794bb1a358d5ec704a96c4a625
Nirmal Singh Thakur <63559214+tawtheme@users.noreply.github.com>
2025-06-26 21:22:12 +0530

    new page "web development"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { FaqsComponent } from './pages/faqs/faqs.component';
+import { WebDevelopmentComponent } from './pages/web-development/web-development.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'faqs', component: FaqsComponent },
+  { path: 'web-development', component: WebDevelopmentComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/pages/web-development/web-development.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-development',
+  imports: [],
+  templateUrl: './web-development.component.html',
+  styleUrl: './web-development.component.scss'
+})
+export class WebDevelopmentComponent {
+
+}
